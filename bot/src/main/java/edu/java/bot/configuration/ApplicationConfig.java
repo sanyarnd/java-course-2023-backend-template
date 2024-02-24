6435006f26e44e6bbbf9af523f3ca1a0462b6111
chulkov-alex <chulkov-alex@yandex-team.ru>
2024-02-24 14:08:45 +0300

    reformat

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,9 +1,7 @@
 package edu.java.bot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.UpdatesListener;
 import jakarta.validation.constraints.NotEmpty;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
