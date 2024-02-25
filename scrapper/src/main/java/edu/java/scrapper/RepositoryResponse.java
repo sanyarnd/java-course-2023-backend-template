a6debf07e9b78391754420f71e6fe86e5d23ca49
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-02-25 14:03:19 +0300

    small changes

--- scrapper/src/main/java/edu/java/scrapper/RepositoryResponse.java
+++ scrapper/src/main/java/edu/java/scrapper/RepositoryResponse.java
@@ -4,18 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.OffsetDateTime;
 
 public record RepositoryResponse(
+    @JsonProperty("id")
     long id,
-
     @JsonProperty("name")
     String repositoryName,
-
-    @JsonProperty("login")
-    String owner,
-
-    @JsonProperty("updated_at")
-    OffsetDateTime updatedAt,
-
     @JsonProperty("pushed_at")
-    OffsetDateTime pushedAt) {
-
+    OffsetDateTime pushedAt
+    ) {
 }
--- scrapper/src/test/java/edu/java/scrapper/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/GitHubClientTest.java
@@ -1,2 +1,52 @@
-package edu.java.scrapper;public class GitHubClientTest {
+package edu.java.scrapper;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.testcontainers.shaded.com.google.common.net.HttpHeaders;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GitHubClientTest {
+    @Rule
+    public final WireMockRule wireMockRule = new WireMockRule();
+    private final String baseUrl = "http://localhost:8080";
+    private GitHubClient gitHubClient;
+    private final String REPOSITORY_PATH = "src" + File.separator + "test" +
+        File.separator + "java" + File.separator + "edu" + File.separator + "java" + File.separator + "scrapper" +
+        File.separator + "repository.json";
+
+    @Before
+    public void initClient() {
+        gitHubClient = new GitHubClient(baseUrl);
+    }
+
+    @Test
+    public void getRepository_shouldGetInfoAboutRepo() throws IOException {
+        //arrange
+        File file = new File(REPOSITORY_PATH);
+        String repositoryJson = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+        String url = "/repos/Minkerr/Tinkoff_Backend_Course_Application";
+        stubFor(get(url)
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withBody(repositoryJson))
+        );
+        //act
+        var act = gitHubClient.getRepository("Minkerr", "Tinkoff_Backend_Course_Application");
+        //assert
+        assertThat(act.id()).isEqualTo(754620859);
+        assertThat(act.repositoryName()).isEqualTo("Tinkoff_Backend_Course_Application");
+        assertThat(act.pushedAt()).isEqualTo("2024-02-23T18:56:38Z");
+    }
+
 }
