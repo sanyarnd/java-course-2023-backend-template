279b30aca70486b91f6c75e7709bb0601e235f85
Valentin Mirontsev <valentin.mironcev@mail.ru>
2024-02-25 16:18:36 +0300

    scheduling added

--- scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -3,10 +3,12 @@ package edu.java.scrapper.configuration;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@EnableScheduling
 public record ApplicationConfig(
     @NotNull
     Scheduler scheduler
