ac64266c5926504db039ee9db52706415fb21572
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-03 10:59:08 +0300

    Add new exception handlers

--- bot/src/main/java/edu/java/bot/client/exception/handle/ExceptionGlobalHandler.java
+++ bot/src/main/java/edu/java/bot/client/exception/handle/ExceptionGlobalHandler.java
@@ -1,22 +1,52 @@
 package edu.java.bot.client.exception.handle;
 
+import edu.java.bot.client.exception.ChatNotFoundException;
+import edu.java.bot.client.exception.LinkAlreadyAddedException;
+import edu.java.bot.client.exception.ScrapperClientException;
+import edu.java.bot.client.exception.UserAlreadyRegisteredException;
 import edu.java.bot.response.ApiErrorResponse;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@Slf4j
-@org.springframework.web.bind.annotation.RestControllerAdvice
+@RestControllerAdvice
 public class ExceptionGlobalHandler {
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> notValidArgumentHandle(MethodArgumentNotValidException ex) {
         return ResponseEntity.status(BAD_REQUEST)
             .body(createError(ex, BAD_REQUEST.getReasonPhrase(), String.valueOf(BAD_REQUEST)));
+    }
+
+    @ExceptionHandler(ChatNotFoundException.class)
+    public ResponseEntity<ApiErrorResponse> chatNotFound(ChatNotFoundException ex) {
+        return ResponseEntity.status(NOT_FOUND)
+            .body(createError(ex, NOT_FOUND.getReasonPhrase(), String.valueOf(NOT_FOUND)));
+    }
+
+    @ExceptionHandler(LinkAlreadyAddedException.class)
+    public ResponseEntity<ApiErrorResponse> linkAlreadyAdded(LinkAlreadyAddedException ex) {
+        return ResponseEntity.status(CONFLICT)
+            .body(createError(ex, CONFLICT.getReasonPhrase(), String.valueOf(CONFLICT)));
+    }
+
+    @ExceptionHandler(ScrapperClientException.class)
+    public ResponseEntity<ApiErrorResponse> scrapperClientError(ScrapperClientException ex) {
+        return ResponseEntity.status(BAD_REQUEST)
+            .body(createError(ex, BAD_REQUEST.getReasonPhrase(), String.valueOf(BAD_REQUEST)));
+    }
+
+    @ExceptionHandler(UserAlreadyRegisteredException.class)
+    public ResponseEntity<ApiErrorResponse> userAlreadyRegistered(UserAlreadyRegisteredException ex) {
+        return ResponseEntity.status(CONFLICT)
+            .body(createError(ex, CONFLICT.getReasonPhrase(), String.valueOf(CONFLICT)));
     }
 
     @ExceptionHandler(Exception.class)
