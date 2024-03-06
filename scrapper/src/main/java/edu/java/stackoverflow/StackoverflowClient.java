6facd4fe2aa7aa169e52c409153c90a536b2b3ba
Denis Karpov <karpovdenis.al@gmail.com>
2024-03-06 19:07:18 +0300

    Change qualifier annotation position

--- scrapper/src/main/java/edu/java/stackoverflow/StackoverflowClient.java
+++ scrapper/src/main/java/edu/java/stackoverflow/StackoverflowClient.java
@@ -6,12 +6,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class StackoverflowClient implements StackoverflowClientInterface {
     private final WebClient webClient;
 
-    public StackoverflowClient(WebClient webClient) {
+    public StackoverflowClient(@Qualifier("stackoverflowClient") WebClient webClient) {
         this.webClient = webClient;
     }
 
     @Override
-    public StackoverflowResponse getStackoverflowResponse(@Qualifier("stackoverflowClient") String questionId) {
+    public StackoverflowResponse getStackoverflowResponse(String questionId) {
         return webClient
             .get()
             .uri("/questions/{questionId}?site=stackoverflow", questionId)
