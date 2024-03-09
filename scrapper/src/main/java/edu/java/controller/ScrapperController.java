eb5f149ce2ea3f1a0be71eab55d0acb953b8b63b
LAPTOP-MR920DE6\nagov <nagovitsyn2003@gmail.com>
2024-03-10 02:31:27 +0300

    hw3 was improved

--- scrapper/src/main/java/edu/java/controller/ScrapperController.java
+++ scrapper/src/main/java/edu/java/controller/ScrapperController.java
@@ -17,7 +17,6 @@ import java.net.URISyntaxException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Validated
 public class ScrapperController {
 
     final private UserTracksService userTracksService;
